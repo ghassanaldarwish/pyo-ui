8208d3cf07c83cdf483aa9d908ca87cac83e4b23
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-08-11 21:45:47 +0200

    fixed the bug

--- src/components/transactions-form/index.tsx
+++ src/components/transactions-form/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DollarSign, Loader } from "lucide-react";
+import { DollarSign } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
